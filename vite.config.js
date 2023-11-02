2a19c11d2cfde1a56aca5b1af746107b247bdefe
klt <letourneurkevin8@gmail.com>
2023-11-02 11:24:16 +0100

    correct base

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Aadvarks-Digital/',
   plugins: [react()],
-});
-
+})
